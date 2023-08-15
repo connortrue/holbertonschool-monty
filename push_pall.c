8af88561d62dcda5040dbc5d4c05feaf626cd7ac
connortrue <connor.true.94@gmail.com>
2023-08-14 23:52:44 -0500

    betty

--- push_pall.c
+++ push_pall.c
@@ -1,34 +1,34 @@
 #include "monty.h"
 void push(stack_t **stack, unsigned int line_number)
 {
-    int value = line_number;
+	int value = line_number;
 
-    stack_t *new_node = malloc(sizeof(stack_t));
-    if (new_node == NULL)
-    {
-        fprintf(stderr, "Error: malloc failed\n");
-        exit(EXIT_FAILURE);
-    }
+	stack_t *new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
 
-    new_node->n = value;
-    new_node->prev = NULL;
-    new_node->next = *stack;
+	new_node->n = value;
+	new_node->prev = NULL;
+	new_node->next = *stack;
 
-    if (*stack != NULL)
-        (*stack)->prev = new_node;
+	if (*stack != NULL)
+		(*stack)->prev = new_node;
 
-    *stack = new_node;
+	*stack = new_node;
 }
 
 void pall(stack_t **stack, unsigned int line_number)
 {
-    stack_t *current = *stack;
-    (void)line_number;
+	stack_t *current = *stack;
+	(void)line_number;
 
-    while (current != NULL)
-    {
-        printf("%d\n", current->n);
-        current = current->next;
-    }
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+	}
 }
 
