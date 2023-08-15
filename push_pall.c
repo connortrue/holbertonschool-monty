b64bf765ff6aa0179591fa13cf192e8e17685959
connortrue <connor.true.94@gmail.com>
2023-08-14 23:48:59 -0500

    meh

--- push_pall.c
+++ push_pall.c
@@ -1,10 +1,9 @@
 #include "monty.h"
 void push(stack_t **stack, unsigned int line_number)
 {
-    // Get the value to push from the `opcodes` array
-    int value = atoi(opcodes[line_number]);
+    int value = line_number;
+    (void)line;
 
-    // Create a new stack node
     stack_t *new_node = malloc(sizeof(stack_t));
     if (new_node == NULL)
     {
@@ -12,22 +11,20 @@ void push(stack_t **stack, unsigned int line_number)
         exit(EXIT_FAILURE);
     }
 
-    // Set the value and pointers of the new node
     new_node->n = value;
     new_node->prev = NULL;
     new_node->next = *stack;
 
-    // Update the previous node's pointer
     if (*stack != NULL)
         (*stack)->prev = new_node;
 
-    // Update the stack head
     *stack = new_node;
 }
 
 void pall(stack_t **stack, unsigned int line_number)
 {
     stack_t *current = *stack;
+    (void)line;
 
     while (current != NULL)
     {
